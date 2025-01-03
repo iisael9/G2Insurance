60c3fd1512e6f49ddd978dc50c91674e469340c0
Isael <iisael090@gmail.com>
2025-01-02 22:19:53 -0800

    modified:   app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -127,6 +127,7 @@ export default function Home() {
       )}
 
       <section id="location-contact" className="p-8 md:p-16 flex flex-wrap gap-8 bg-[#F8EEEE]">
+        <h2 className="text-3xl md:text-4xl font-bold text-center w-full mb-6">Location & Contact</h2>
         <div className='rounded-2xl p-6 bg-white shadow-lg flex flex-col gap-6 flex-1'>
           <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d206.56224702158576!2d-117.57852742718241!3d34.06961801767697!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c335a0d776616f%3A0xc8b866bfc4088f5c!2s3350%20Shelby%20St%20%23200%2C%20Ontario%2C%20CA%2091764!5e0!3m2!1sen!2sus!4v1729128388087!5m2!1sen!2sus" width="100%" height="264" className='shadow-xl' loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
           <div className="flex gap-2 items-center">
@@ -188,7 +189,7 @@ export default function Home() {
         </div>
       </section>
 
-      <footer className="p-6 bg-[#CD252B] text-white text-center md:flex md:justify-between md:items-center">
+      <footer className="p-6 bg-[#F8EEEE] text-black text-center md:flex md:justify-between md:items-center">
         <p className="text-sm md:text-lg">© 2025 G2 Business & Auto Insurance. All rights reserved.</p>
         <nav className="flex justify-center gap-4 mt-4 md:mt-0">
           <a href="#home" className="hover:underline">Home</a>
