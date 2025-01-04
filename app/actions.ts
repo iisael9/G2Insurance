c6780eb7f17edb712be2b5fd9a8df460eef852b5
iisael9 <iisael090@gmail.com>
2025-01-03 19:47:39 -0800

    modified:   app/actions.ts

--- app/actions.ts
+++ app/actions.ts
@@ -1,7 +1,7 @@
 'use server'
 import { Resend } from 'resend';
 import EmailTemplate from './components/email-template';
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.g2ins);
 
 export default async function sendEmail(fd: FormData) {
     const formData = Object.fromEntries(fd);
